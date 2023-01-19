fcffa57e28a8d6e1f6815cbbb68cad2e5d86a198
Gustavo Salgado Lima <gustavosalgadolima@gmail.com>
2023-01-19 19:22:10 -0300

    swagger config

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using api.Repositorios.Entity;
+using Microsoft.OpenApi.Models;
 using Radar_Api.Models;
 using Radar_Api.Repositories;
 using Radar_Api.Repositorios.Interfaces;
@@ -17,7 +18,33 @@ builder.Services.AddScoped<IServico<PosicaoProduto>, PosicaoProdutoRepository>()
 builder.Services.AddScoped<IServico<Campanha>, CampanhaRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "API Radar", Version = "v1" });
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 
 var app = builder.Build();
 
