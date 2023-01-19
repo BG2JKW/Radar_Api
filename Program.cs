96b198eff12db4e72b5bf26f032afd8a4130a60a
Gustavo Salgado Lima <gustavosalgadolima@gmail.com>
2023-01-18 22:00:52 -0300

    cross domain

--- Program.cs
+++ Program.cs
@@ -28,6 +28,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+);
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
