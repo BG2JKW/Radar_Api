a9fe47809ae49bbbe9dc3f754a8aa3d3ca42d8d8
wcocuzza <wallace.cocuzza@gmail.com>
2023-01-21 21:24:48 -0300

    Adicionado paginação ao get das rotas

--- Controllers/LojasController.cs
+++ Controllers/LojasController.cs
@@ -18,10 +18,12 @@ public class LojasController : ControllerBase
     [HttpGet("")]
     [Authorize(Roles = "adm,editor")]
     
-    public async Task<IActionResult> Index()
+    public async Task<IActionResult> Index(
+        [FromQuery] int skip = 0,
+        [FromQuery] int take = 10)
     {
         var lojas = await _servico.TodosAsync();
-        return StatusCode(200, lojas);
+        return StatusCode(200, lojas.Skip(skip).Take(take));
     }
 
     [HttpGet("{id}")]
